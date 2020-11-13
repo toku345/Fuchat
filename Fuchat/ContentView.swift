f536a6b5dc8d10613f0c0b2dd708bdd09119f216
Fumitaka Tokumitsu <toku345@gmail.com>
2020-11-13 18:05:10 +0900

    Initial Commit

---
+++ Fuchat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fuchat
+//
+//  Created by Fumitaka Tokumitsu on 2020/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fuchat/FuchatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FuchatApp.swift
+//  Fuchat
+//
+//  Created by Fumitaka Tokumitsu on 2020/11/13.
+//
+
+import SwiftUI
+
+@main
+struct FuchatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
